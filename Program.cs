63ba2de0b94c9d4cbb4e450056fc2d5969d8913e
Your Name <Ana>
2020-02-17 11:14:23 -0700

    practica7

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prcatica7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        bool isFizzOrBuzz = false;
+        string fizzBuzzMessage = "";
+
+            for (int i = 1; i <= 100; i++) {
+                isFizzOrBuzz = false;
+                fizzBuzzMessage = "";
+
+                if (i % 3 == 0) {
+                    fizzBuzzMessage += "Fizz";  // Console.Write("Fizz");
+                    isFizzOrBuzz = true;
+                }
+                if (i % 5 == 0) {
+                    fizzBuzzMessage += "Buzz";  // Console.Write("Buzz");
+                    isFizzOrBuzz = true;
+                }
+
+                Console.Write(isFizzOrBuzz ? fizzBuzzMessage : i.ToString());
+                Console.WriteLine("");
+            }
+        }
+    }
+}
